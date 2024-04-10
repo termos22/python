7fbc81b413eb32c26d2e9c112e889592ec1bc162
Jakub Przybysz <termos22@gmail.com>
2024-04-10 17:51:08 +0200

    done dictionaries exercise

---
+++ course_advanced/9.94_ex28_introduction_to_dict.py
@@ -0,0 +1,10 @@
+user_var = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five"}
+print(user_var[3])
+print(6 in user_var)
+print(0 not in user_var)
+
+# example solution
+dictionary = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
+print(dictionary["c"])
+print("a" in dictionary)
+print("b" not in dictionary)
