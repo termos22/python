1f4f76c3792afabd10f305713a82eb433878b716
Jakub Przybysz <termos22@gmail.com>
2024-05-06 00:58:26 +0200

    done the dict function

---
+++ course_advanced/9.106_dict_function.py
@@ -0,0 +1,10 @@
+empty = dict()
+print(empty)
+
+with_values = dict(a1=1, b_=2, c_3=3)
+print(with_values)
+
+# ograniczenia w używaniu funkcji dict()
+# -klucz musi zaczynac sie od litery, niedopuszczalne jest aby na początku
+#  znajdowała się cyfra
+# -klucz może składać się jedynie z liter, cyfr i znaku podłogi 
