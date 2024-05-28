78af45c45dd2d107e7f6a82ee3989b487a11ae4e
Jakub Przybysz <termos22@gmail.com>
2024-05-28 18:47:59 +0200

    created set comprehensions with lloops

---
+++ course_advanced/11.112_set_comprehensions.py
@@ -0,0 +1,34 @@
+# set coprehensions
+
+comp_1 = {even+2 for even in range(2, 11, 2)}
+print(comp_1)
+
+comp_2 = {char.lower() for char in "ALLCAPS"}
+print(comp_2)
+
+
+
+
+
+
+# Some coders have developed the habit of putting constants first in '==' comparisons. 
+# If '=' were accidentally typed, this makes the constant an l-value in an assignment 
+# and the compiler or interpreter would complain, drawing the coder's attention 
+# to the error.
+
+
+
+
+# Interesting point, putting the variable on the right side of a comparison would prevent
+# accidental assignment.
+# It's rare that I get a student who is an experienced programmer and can help me improve
+# the course, so your feedback has been very valuable. I am currently working on 
+# remastering the course. That includes re-filming the course videos and creating 
+# additional review exercises and programming challenges.  I also plan on making videos
+# going over the exercises and programming challenges, so it will be a while until 
+# the remaster is complete. I expect to be finished no later than the end of October
+# and will make an announcement about it a week before I expect to have it completed. 
+
+# In the meantime, I will make any helpful comments you make into featured questions 
+# so that I can find them easier and make changes to my lectures if necessary during 
+# the re-filming process.
