41ce06da6d547e4dbcf4c3e76c2afee710d924d7
Jakub Przybysz <termos22@gmail.com>
2025-01-20 01:24:08 +0100

    nowy skrypt odwracanie tekstu

---
+++ tutorials/reverse_text.py
@@ -0,0 +1,9 @@
+def odwroc_slowo(tekst)
+    ### odwraca kolejność liter w tekście 
+    return tekst[::-1]
+
+def main():
+    print("=" *50)
+    print("Witaj w magicznym odwracaczu słów")
+    print("Zamień dowolny tekst w jego lustrzane odbicie")
+    print("=" *50)
