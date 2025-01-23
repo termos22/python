be7c44aeac44ff7fe6cbe38a426ff99ff3517d33
Jakub Przybysz <termos22@gmail.com>
2025-01-23 18:46:11 +0100

    poprawiamy kod zegara bo jest zjebany

--- tutorials/digital_clock.py
+++ tutorials/digital_clock.py
@@ -7,6 +7,17 @@ def wyswietl_czas():
         print("\033[H\033[J", end="")
         print("=" * 50)
         print("super zegar cyfrowy w Pythonie")
-        print("=" * 50)d}:{teraz.second:     }")
+        print("=" * 50),{teraz.second}")
         print(f"{teraz.day:02d}.{teraz.month:02d}.{teraz.year}")
-        
+        print("\n" + "=" * 50)
+        print("zegar stworzony w kilku linijkach kodu")
+        print("=" * 50)
+        time.sleep(1)
+
+def main():
+    try:
+        wyswietl_czas()
+    except KeyboardInterrupt:
+        print("\n\n dziękujemy za skorzystanie z zegara")
+if __name__ == "__main__":
+    main()
